82b70808628e1447871d7dac4358b404ac132989
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-08-04 00:57:31 +0900

    feat(joystick): 試験的に`/cmd/**`トピックを吐く

--- scripts/joystick.py
+++ scripts/joystick.py
@@ -7,15 +7,19 @@ import sys
 import pygame
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Joy
-from std_msgs.msg import Header
+from geometry_msgs.msg import Vector3
+from std_msgs.msg import Header, Bool
 
 
 class Joystick(Node):
     def __init__(self):
         super().__init__("joystick")
         self._timer = self.create_timer(0.05, self._timer_callback)
-        self._joy_publisher = self.create_publisher(Joy, "joystick", 10)
+
+        self._orientation_publisher = self.create_publisher(Vector3, "/cmd/target_orientation", 10)
+        self._velocity_publisher = self.create_publisher(Vector3, "/cmd/target_velocity", 10)
+        self._indicator_led_publisher = self.create_publisher(Bool, "/cmd/indicator_led_enabled", 10)
+
         # TODO : ここparameterで変えられるようにしたい
         self._joystick = pygame.joystick.Joystick(0)
         # logging
@@ -39,8 +43,23 @@ class Joystick(Node):
 
         axes = [self._joystick.get_axis(i) for i in range(numaxes)]
         buttons = [int(self._joystick.get_button(i)) for i in range(numbuttons)]
-        msg = Joy(axes=axes, buttons=buttons, header=self._generate_header())
-        self._joy_publisher.publish(msg)
+        # 左スティック
+        velocity_msg = Vector3(
+            x = axes[0],
+            y = -axes[1],
+            z = 0
+        )
+        # 右スティック
+        orientation_msg = Vector3(
+            x = axes[3],
+            y = -axes[4],
+            z = 0
+        )
+        # 丸ボタン
+        indicator_led_msg = Bool(data=bool(buttons[1]))
+        self._velocity_publisher.publish(velocity_msg)
+        self._orientation_publisher.publish(orientation_msg)
+        self._indicator_led_publisher.publish(indicator_led_msg)
 
 
 def main(args=sys.argv):
