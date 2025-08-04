ca12f982312c8be13d15772af7d1db6b4156777f
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-08-04 14:59:33 +0900

    style(joystick): ruff format

--- scripts/joystick.py
+++ scripts/joystick.py
@@ -13,41 +13,39 @@ from std_msgs.msg import Bool
 
 class Joystick(Node):
     def __init__(self):
-        super().__init__("joystick")
+        super().__init__('joystick')
         self._timer = self.create_timer(0.05, self._timer_callback)
 
-        self._orientation_publisher = self.create_publisher(Vector3, "/cmd/target_orientation", 10)
-        self._velocity_publisher = self.create_publisher(Vector3, "/cmd/target_velocity", 10)
-        self._indicator_led_publisher = self.create_publisher(Bool, "/cmd/indicator_led_enabled", 10)
+        self._orientation_publisher = self.create_publisher(
+            Vector3, '/cmd/target_orientation', 10
+        )
+        self._velocity_publisher = self.create_publisher(
+            Vector3, '/cmd/target_velocity', 10
+        )
+        self._indicator_led_publisher = self.create_publisher(
+            Bool, '/cmd/indicator_led_enabled', 10
+        )
 
         # TODO : ここparameterで変えられるようにしたい
         self._joystick = pygame.joystick.Joystick(0)
         # logging
         name = self._joystick.get_name()
-        self.get_logger().info(f"Using controller: {name}")
+        self.get_logger().info(f'Using controller: {name}')
 
     def _timer_callback(self):
-        self.get_logger().debug("tick")
+        self.get_logger().debug('tick')
         pygame.event.pump()
 
         numaxes = self._joystick.get_numaxes()
         numbuttons = self._joystick.get_numbuttons()
-        self.get_logger().info(f"Found {numaxes} axes and {numbuttons} buttons")
+        self.get_logger().info(f'Found {numaxes} axes and {numbuttons} buttons')
 
         axes = [self._joystick.get_axis(i) for i in range(numaxes)]
         buttons = [int(self._joystick.get_button(i)) for i in range(numbuttons)]
         # 左スティック
-        velocity_msg = Vector3(
-            x = axes[0],
-            y = -axes[1],
-            z = 0.0
-        )
+        velocity_msg = Vector3(x=axes[0], y=-axes[1], z=0.0)
         # 右スティック
-        orientation_msg = Vector3(
-            x = axes[3],
-            y = -axes[4],
-            z = 0.0
-        )
+        orientation_msg = Vector3(x=axes[3], y=-axes[4], z=0.0)
         # 丸ボタン
         indicator_led_msg = Bool(data=bool(buttons[1]))
         self._velocity_publisher.publish(velocity_msg)
@@ -65,5 +63,5 @@ def main(args=sys.argv):
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
