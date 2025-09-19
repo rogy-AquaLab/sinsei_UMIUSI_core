61c3daf691d12b4b092906aaaa1aff866b5850c8
Chikurin1014 <chikurin1014@gmail.com>
2025-09-19 14:14:43 +0000

    :fire: revert sample codes

--- src/sinsei_umiusi_core/behaviortree/ros_sync_action_node.cpp
+++
@@ -1,7 +0,0 @@
-#include "sinsei_umiusi_core/behaviortree/ros_sync_action_node.hpp"
-
-sinsei_umiusi_core::behavortree::RosSyncActionNode::RosSyncActionNode(
-  const std::string & name, const BT::NodeConfiguration & config, rclcpp::Node::SharedPtr node)
-: BT::SyncActionNode(name, config), node(node)
-{
-}
