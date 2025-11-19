a98403368f98a8eabc878eca5a4668a64bb7eeb3
Chikurin1014 <chikurin1014@gmail.com>
2025-11-19 16:51:31 +0000

    feat(robot_state): :construction: add clients for lifecycle_msgs/srv/change_state

--- include/sinsei_umiusi_core/robot_state/robot_state.hpp
+++ include/sinsei_umiusi_core/robot_state/robot_state.hpp
@@ -4,8 +4,11 @@
 #include <behaviortree_cpp/bt_factory.h>
 
 #include <memory>
+#include <rclcpp/client.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <string_view>
+
+#include "lifecycle_msgs/srv/change_state.hpp"
 
 namespace sinsei_umiusi_core::robot_state
 {
@@ -14,14 +17,20 @@ class RobotState : public rclcpp::Node
 {
   public:
     static constexpr std::string_view PARAM_NAME_BEHAVIOR_TREE_FILE = "behavior_tree_file";
-    RobotState();
-    ~RobotState() = default;
 
   private:
     rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedPtr
+      change_state_manual_target_generator;
+    rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedPtr change_state_auto_target_generator;
+    // rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedPtr change_state_debug_thruster_output;
     std::unique_ptr<BT::Tree> tree;
 
     auto timer_callback() const -> void;
+
+  public:
+    RobotState();
+    ~RobotState() = default;
 };
 
 }  // namespace sinsei_umiusi_core::robot_state
--- src/sinsei_umiusi_core/robot_state/robot_state.cpp
+++ src/sinsei_umiusi_core/robot_state/robot_state.cpp
@@ -3,21 +3,22 @@
 #include <behaviortree_cpp/bt_factory.h>
 
 #include <functional>
+#include <lifecycle_msgs/msg/transition.hpp>
+#include <lifecycle_msgs/srv/change_state.hpp>
 #include <memory>
 #include <rcl_interfaces/msg/parameter_descriptor.hpp>
+#include <rclcpp/executors.hpp>
 #include <rclcpp/parameter_value.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <string>
 
-#include "rcl_interfaces/msg/parameter_descriptor.hpp"
+#include "sinsei_umiusi_core/robot_state/power_off.hpp"
 
 using namespace std::chrono_literals;
 
 sinsei_umiusi_core::robot_state::RobotState::RobotState()
-: rclcpp::Node("robot_state_node"), timer(nullptr), tree(nullptr)
+: rclcpp::Node("robot_state"), timer(nullptr), tree(nullptr)
 {
-    RCLCPP_INFO(this->get_logger(), "RobotState node has been started.");
-
     this->declare_parameter(
       std::string(PARAM_NAME_BEHAVIOR_TREE_FILE), "unknown",
       rcl_interfaces::msg::ParameterDescriptor()
@@ -25,26 +26,66 @@ sinsei_umiusi_core::robot_state::RobotState::RobotState()
         .set__read_only(true)
         .set__type(rclcpp::ParameterType::PARAMETER_STRING));
 
-    this->timer =
-      this->create_wall_timer(1s, std::bind(&RobotState::timer_callback, this), nullptr, false);
+    {
+        auto factory = BT::BehaviorTreeFactory();
+        // factory.registerNodeType<YourCustomNode>("YourCustomNode");
+        factory.registerNodeType<sinsei_umiusi_core::robot_state::PowerOff>("PowerOff");
+        auto tree = factory.createTreeFromFile(
+          this->get_parameter(std::string(PARAM_NAME_BEHAVIOR_TREE_FILE)).as_string());
+        this->tree = std::make_unique<BT::Tree>(std::move(tree));
+    }
+    this->change_state_manual_target_generator =
+      this->create_client<lifecycle_msgs::srv::ChangeState>(
+        "/manual_target_generator/change_state");
+    this->change_state_auto_target_generator =
+      this->create_client<lifecycle_msgs::srv::ChangeState>("/auto_target_generator/change_state");
+    {
+        // Request to configure managed nodes
+        auto req = std::make_shared<lifecycle_msgs::srv::ChangeState::Request>();
+        req->set__transition(lifecycle_msgs::msg::Transition{}.set__id(
+          lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE));
 
-    auto factory = BT::BehaviorTreeFactory();
-    // factory.registerNodeType<YourCustomNode>("YourCustomNode");
-    auto tree = factory.createTreeFromFile(
-      this->get_parameter(std::string(PARAM_NAME_BEHAVIOR_TREE_FILE)).as_string());
-    this->tree = std::make_unique<BT::Tree>(std::move(tree));
+        this->change_state_manual_target_generator->wait_for_service();
+        this->change_state_auto_target_generator->wait_for_service();
+        // this->change_state_debug_thruster_output->wait_for_service();
 
-    this->timer->reset();
+        auto future_manual = this->change_state_manual_target_generator->async_send_request(req);
+        auto future_auto = this->change_state_auto_target_generator->async_send_request(req);
+        // auto future_debug = this->change_state_debug_thruster_output->async_send_request(req);
+
+        auto result_manual =
+          rclcpp::spin_until_future_complete(this->get_node_base_interface(), future_manual);
+        auto result_auto =
+          rclcpp::spin_until_future_complete(this->get_node_base_interface(), future_auto);
+        // auto result_debug =
+        //   rclcpp::spin_until_future_complete(this->get_node_base_interface(), future_debug);
+        if (
+          result_manual != rclcpp::FutureReturnCode::SUCCESS ||
+          result_auto != rclcpp::FutureReturnCode::SUCCESS
+          // || result_debug != rclcpp::FutureReturnCode::SUCCESS
+        ) {
+            RCLCPP_ERROR(
+              this->get_logger(), "Failed to call service change_state on manual_target_generator");
+        }
+    }
+    this->timer = this->create_wall_timer(1s, std::bind(&RobotState::timer_callback, this));
 }
 
 auto sinsei_umiusi_core::robot_state::RobotState::timer_callback() const -> void
 {
-    RCLCPP_INFO(this->get_logger(), "Timer callback triggered");
     if (!tree) {
         RCLCPP_WARN(this->get_logger(), "Behavior tree is not initialized.");
         return;
     }
     BT::NodeStatus status = tree->rootNode()->executeTick();
-    RCLCPP_INFO(
-      this->get_logger(), "Behavior tree tick executed with status: %s", BT::toStr(status).c_str());
+    switch (status) {
+        case BT::NodeStatus::SUCCESS:
+            RCLCPP_INFO(this->get_logger(), "Behavior tree finished with SUCCESS.");
+            break;
+        case BT::NodeStatus::FAILURE:
+            RCLCPP_ERROR(this->get_logger(), "Behavior tree finished with FAILURE.");
+            break;
+        default:
+            break;
+    }
 }
