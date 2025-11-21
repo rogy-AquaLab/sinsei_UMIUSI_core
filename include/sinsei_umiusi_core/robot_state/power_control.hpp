ab0be1bc19e993793712200cfeee2485aaee6a71
Chikurin1014 <chikurin1014@gmail.com>
2025-11-21 08:52:06 +0000

    chore: :adhesive_bandage: fix encoding

---
+++ include/sinsei_umiusi_core/robot_state/power_control.hpp
@@ -0,0 +1,28 @@
+#ifndef SINSEI_UMIUSI_CORE_ROBOT_STATE_POWER_CONTROL_HPP
+#define SINSEI_UMIUSI_CORE_ROBOT_STATE_POWER_CONTROL_HPP
+
+#include <behaviortree_cpp/action_node.h>
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace sinsei_umiusi_core::robot_state
+{
+
+class PowerControl : public BT::StatefulActionNode
+{
+  private:
+    rclcpp::Node::SharedPtr ros_node;
+
+  public:
+    PowerControl(const std::string & name, const BT::NodeConfiguration & config);
+
+    static BT::PortsList providedPorts() { return {}; }
+
+    auto onStart() -> BT::NodeStatus override;
+    auto onRunning() -> BT::NodeStatus override;
+    auto onHalted() -> void override;
+};
+
+}  // namespace sinsei_umiusi_core::robot_state
+
+#endif  // SINSEI_UMIUSI_CORE_ROBOT_STATE_POWER_CONTROL_HPP
